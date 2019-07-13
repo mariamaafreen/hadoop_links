74d5fbe6edb463cac10e58317fb6cc3e556a61a2
Mariam <sanubia99@gmail.com>
2019-07-13 18:41:45 +0530

    hadoop

--- app.py
+++ app.py
@@ -10,4 +10,5 @@ def home():
 
     return render_template('view.html', datas = c.fetchall())
 
-app.run(debug=True)
+if __name__ =='__main__':
+    app.run(debug=True)
--- url.py
+++ url.py
@@ -1,7 +1,7 @@
 import bs4
 
 import requests
-url = "https://prwatech.in/blog/hadoop/hadoop-basic-linux-commands/"
+url = "https://www.tutorialspoint.com/articles/apache-hadoop-and-its-components"
 data = requests.get(url)
 soup = bs4.BeautifulSoup(data.text, 'html.parser')
 
@@ -9,8 +9,8 @@ soup = bs4.BeautifulSoup(data.text, 'html.parser')
 for para in soup.find_all('a'):
     link = para.get("href")
     if link[0:3] =="../" and link!="#":
-        print("https://prwatech.in"+link[2:len(link)])
+        print("https://www.tutorialspoint.com"+link[2:len(link)])
     elif link[0]=="/" and link!="#":
-        print("https://prwatech.in"+link)
+        print("https://www.tutorialspoint.com"+link)
     elif link!="#":
         print(link)
